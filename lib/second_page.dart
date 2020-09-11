862338751c4ac6ad9270565a6d37b8f8db48d68c
DanielBarragan96 <ie702554@iteso.mx>
2020-09-11 11:03:10 -0500

    se agrega gradiente a pagina 2

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -20,44 +20,55 @@ class _SecondPageState extends State<SecondPage> {
         title: Text('Pagina 2'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Container(
-              child: Text(
-                'Genere numero random',
-                style: Theme.of(context).textTheme.headline3,
-              ),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue, Colors.white],
             ),
-            SizedBox(height: 10),
-            Container(
-              child: Text(
-                '185',
-                style: Theme.of(context).textTheme.bodyText2,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(height: 20),
+              Container(
+                child: Text(
+                  'Genere numero random',
+                  style: Theme.of(context).textTheme.headline3,
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            MaterialButton(
-              onPressed: () {},
-              child: Text(
-                "Generar",
-                style: Theme.of(context).textTheme.button,
+              SizedBox(height: 10),
+              Container(
+                child: Text(
+                  '185',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
               ),
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-            ),
-            MaterialButton(
-              onPressed: () {},
-              child: Text(
-                "Guardar",
-                style: Theme.of(context).textTheme.button,
+              SizedBox(height: 10),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  "Generar",
+                  style: Theme.of(context).textTheme.button,
+                ),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
               ),
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-            ),
-          ],
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  "Guardar",
+                  style: Theme.of(context).textTheme.button,
+                ),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
